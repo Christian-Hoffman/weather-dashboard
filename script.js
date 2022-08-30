5e306cc399f815b77ca9f8c855c4490a2850daa5
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-08-29 21:06:52 -0400

    added click event to search button

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var appid = '692efab00ae66e9f48137e6ea4766fcd';
-var q = 'Chicago';
-var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`
+var q = document.querySelector('#q');
+var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q.value}&appid=${appid}`;
+
+var buttonEl = document.querySelector('#button');
 
 var toJSON = function(response) {
     return response.json();
 };
 
+// takes city and weather data from city and appends it to the page
 var showWeather = function(data, city) {
     console.log(data);
     var currentEl = document.querySelector('#current');
@@ -17,8 +20,9 @@ var showWeather = function(data, city) {
     currentEl.appendChild(pEl);
 };
 
-var getOneCall = function(city) {
-    var oneCall = `http://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&appid=${appid}&units=imperial`
+// uses the oneCall api tp gather weather data from given city
+var getOneCall = function(data, city) {
+    var oneCall = `http://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&appid=${appid}&units=imperial`;
     fetch(oneCall)
     .then(toJSON)
     .then(function(data) {
@@ -26,13 +30,20 @@ var getOneCall = function(city) {
     });
 };
 
+// console logs lat and lon. Calls oneCall function
 var getGEO = function(locations) {
     var city = locations[0];
-    console.log('Lat', city.lat);
-    console.log('Lon', city.lon);
+    console.log('LAT', city.lat);
+    console.log('LON', city.lon);
     getOneCall(city);
 };
 
-fetch(geoURL)
-    .then(toJSON)
-    .then(getGEO);
+// function for event listener to call
+var citySearch = function(event) {
+    event.preventDefault();
+    fetch(geoURL)
+        .then(toJSON)
+        .then(getGEO);    
+}
+
+buttonEl.addEventListener('click', citySearch);
